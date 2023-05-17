afadbd76ecfffdc5d801423c13a9a6f2ee9f1d48
Katrin00 <rfnhbyf2000@mail.ru>
2023-05-17 21:23:19 +0300

    Add new classes

---
+++ src/src/Mood.java
@@ -0,0 +1,20 @@
+public class Mood {
+    private String title;
+
+    Mood() {
+        this.title = "unset";
+    }
+
+    Mood(String title) {
+        this.title = title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+}
